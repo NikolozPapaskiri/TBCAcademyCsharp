deb641afd0032b1eac93f7cbd22790c23492f87c
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-05 02:09:38 +0400

    Task 03 (Lucky Year Name) is done

---
+++ Assignment 01/Task_03/Task_03/LuckyYear.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    public class LuckyYear
+    {
+        public static string GetLuckyYearName(int year)
+        {
+            switch (year % 12)
+            {
+                case 0:
+                    return "Monkey";
+                case 1:
+                    return "Rooster";
+                case 2:
+                    return "Dog";
+                case 3:
+                    return "Pig";
+                case 4:
+                    return "Rat";
+                case 5:
+                    return "Ox";
+                case 6:
+                    return "Tiger";
+                case 7:
+                    return "Rabbit";
+                case 8:
+                    return "Dragon";
+                case 9:
+                    return "Snake";
+                case 10:
+                    return "Horse";
+                case 11:
+                    return "Sheep";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
