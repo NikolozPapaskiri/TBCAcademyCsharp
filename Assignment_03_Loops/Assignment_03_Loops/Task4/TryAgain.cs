e60679af595b564cf7dc983ce8ea24db8a2d266d
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-07 17:20:01 +0400

    Task 4 (pyramid) is done

---
+++ Assignment_03_Loops/Assignment_03_Loops/Task4/TryAgain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class TryAgain
+    {
+        public static bool Ask()
+        {
+            Console.Write("\nDo you want to play again? (y/n): ");
+            string input = Console.ReadLine();
+            return input.Equals("y", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
