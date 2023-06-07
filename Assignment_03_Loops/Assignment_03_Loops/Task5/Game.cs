8355be24a930b9649a13db0f0c62a1c6d0ab6916
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-07 18:30:49 +0400

    task 5 (guessing the number) is done

---
+++ Assignment_03_Loops/Assignment_03_Loops/Task5/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class Game
+    {
+        public static void PlayGame(int min, int max)
+        {
+            Random random = new Random();
+            int randNum = random.Next(min, max);
+
+            while (true)
+            {
+                Console.Write("Guess the number: ");
+
+                if (int.TryParse(Console.ReadLine(), out int num) && num > -1)
+                {
+                    if(num == randNum)
+                    {
+                        Console.WriteLine($"Congrats, you guessed the number! It was {randNum}");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a positive integer");
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assignment_03_Loops/Assignment_03_Loops/Task5/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task5
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool play = true;
+
+            while (play)
+            {
+
+                Console.Write("\nEnter 1st number: ");
+
+                if (int.TryParse(Console.ReadLine(), out int firstNumber) && firstNumber >= 0)
+                {
+                    while (true) //ამ ვაილში ვატრიალებ რო მე2 რიცხვის შეყვანისას ვალიდაციის დარტყმაზე მარტო მე2 რიცხვი შეგაყვანინოს თავიდან და არა 1ლიც
+                    {
+                        Console.Write("Enter 2nd number: ");
+
+                        if (int.TryParse(Console.ReadLine(), out int secondNumber) && secondNumber > firstNumber)
+                        {
+                            Console.WriteLine();
+                            Game.PlayGame(firstNumber, secondNumber);
+
+                            if (!TryAgain.Ask())
+                            {
+                                play = false;//ამითი 2ვე ვაილიდან გამოვა და თამაშს მორჩება
+                                break;
+                            }
+                            else
+                            {
+                                break;//ამითი ამ ვაილიდან გამოვა და თამაშს თავიდან დაიწყებს
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a positive integer that is more than first number.");
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a positive integer.");
+                }
+            }
+        }
+    }
+}
