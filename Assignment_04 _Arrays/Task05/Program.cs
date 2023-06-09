72869a13d17270bfb5ccbef1b810c37b26b97416
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-09 20:45:06 +0400

    Task05 (determinant calculator) is done

---
+++ Assignment_04 _Arrays/Task05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("== Determinant Calculator ==");
+
+            int choice = UserInputUtils.GetChoice("Choose matrix size:\n1. 2 x 2\n2. 3 x 3\n");
+
+            int[,] matrix;
+
+            if (choice == 1)
+            {
+                matrix = UserInputUtils.GetMatrixElements(2, 2);
+            }
+            else
+            {
+                matrix = UserInputUtils.GetMatrixElements(3, 3);
+            }
+
+            Console.WriteLine("You entered matrix:");
+            MatrixUtils.PrintMatrix(matrix);
+
+            int determinant = MatrixUtils.CalculateDeterminant(matrix);
+            Console.WriteLine("Determinant is: " + determinant);
+        }
+    }
+
+}
---
+++ Assignment_04 _Arrays/Task05/UserInputUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    public static class UserInputUtils
+    {
+        public static int GetChoice(string message)
+        {
+            int choice = default;
+            bool isValidChoice = false;
+
+            while (!isValidChoice)
+            {
+                Console.Write(message);
+                string choiceInput = Console.ReadLine();
+                isValidChoice = int.TryParse(choiceInput, out choice);
+
+                if (!isValidChoice)
+                {
+                    Console.WriteLine("Invalid choice. Please enter a valid integer.");
+                }
+            }
+
+            return choice;
+        }
+
+        public static int[,] GetMatrixElements(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            Console.WriteLine("Enter matrix elements:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    bool isValidNumber = false;
+
+                    while (!isValidNumber)
+                    {
+                        Console.Write($"[{i}][{j}]: ");
+                        string numberInput = Console.ReadLine();
+
+                        if (!int.TryParse(numberInput, out int number))
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid integer.");
+                        }
+                        else
+                        {
+                            matrix[i, j] = number;
+                            isValidNumber = true;
+                        }
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
