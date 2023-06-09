17fa1c623bb2548b6af1ae8e4d6cacdbdaee47b3
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-09 19:10:24 +0400

    Task 1 (SortArray) is done

---
+++ Assignment_04 _Arrays/Task01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the size of the array: ");
+            int size;
+
+            while (!int.TryParse(Console.ReadLine(), out size) || size < 1)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid positive integer.");
+                Console.Write("Enter the size of the array: ");
+            }
+
+            int[] arr = new int[size];
+
+            Console.WriteLine($"Enter {size} numbers:");
+
+            for (int i = 0; i < size; i++)
+            {
+                int num;
+
+                Console.Write($"Enter number {i + 1}: ");
+                while (!int.TryParse(Console.ReadLine(), out num))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    Console.Write($"Enter number {i + 1}: ");
+                }
+
+                arr[i] = num;
+            }
+
+
+            ArraySorter.SortArray(arr);
+
+            Console.WriteLine("\nSorted array:");
+
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
