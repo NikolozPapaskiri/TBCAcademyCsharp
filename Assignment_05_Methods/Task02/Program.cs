e1cfff790ba71e91edce2fe9797d1c4c9abbff61
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-12 03:02:39 +0400

    just added some line indentations

--- Assignment_05_Methods/Task02/Program.cs
+++ Assignment_05_Methods/Task02/Program.cs
@@ -10,16 +10,19 @@ namespace Task02
 
             //მოსაძებნი სიმბოლოს ინფუთი ცალკე მქონდა მარა ბოლოს Count() მეთოდში მომინდა გადამეცა ესე ფუნქციათ მეორე პარამეტრში და ასევე მესამე პარამეტრად out char symbol დავამატე რო Mainშიც მივწვდე და ვიცოდე ამ ფუნქციაში შეყვანილი სიმბოლო
             //მაინც მეთოდების და ფუნქციების დავალებაა :დ
-            int count = CountOccurrencesInArray.Count(chars, () =>
-            {
-                char symbol;
-                Console.Write("Enter symbol to count in the array: ");
-                while (!char.TryParse(Console.ReadLine(), out symbol))
-                {
-                    Console.Write("Invalid input. Please enter a single character: ");
-                }
-                return symbol;
-            }, out char symbol);
+            //მგონი რო count-ამდე მეინში გაკეთება ამ ყველაფრის წასაკითხად უფრო მარტივი იქნება მარა მაინც
+            int count = CountOccurrencesInArray.Count(chars 
+                                                        ,() =>
+                                                            {
+                                                                char symbol;
+                                                                Console.Write("Enter symbol to count in the array: ");
+                                                                while (!char.TryParse(Console.ReadLine(), out symbol))
+                                                                {
+                                                                    Console.Write("Invalid input. Please enter a single character: ");
+                                                                }
+                                                                return symbol;
+                                                            }
+                                                        ,out char symbol);
 
             PrintResult.Print(symbol, count);
         }
