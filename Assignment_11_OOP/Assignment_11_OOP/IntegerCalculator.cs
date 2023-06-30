308bf5aff36f6651ed7e3caa461f69987d63081c
Nikoloz Papaskiri <nkpapaskiri@gmail.com>
2023-06-30 19:57:27 +0400

    Task 1 is done

---
+++ Assignment_11_OOP/Assignment_11_OOP/IntegerCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11_OOP
+{
+    public class IntegerCalculator : ICalculator<int>
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int ReadInput(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+        }
+    }
+}
